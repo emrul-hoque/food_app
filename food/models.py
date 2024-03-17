0e42a547a9a4715b39b0f98e5547ea86b8637782
Emrul <emrul.hoque@gmail.com>
2024-03-17 21:21:32 +0600

    Fixed form alignments

--- food/models.py
+++ food/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
